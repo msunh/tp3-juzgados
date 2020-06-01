79f1d74e105ad46de2771a3ecaa6b03f4090d345
msunh <msalto.unh@gmail.com>
2020-06-01 19:34:38 -0300

    hasta en juicio

---
+++ Expedientes_Juzgados.py
@@ -0,0 +1,168 @@
+from Colas import *
+from Pila import *
+from Fuero import *
+
+class Expediente :
+    
+    def __init__(self, nroExp = 0 ,fuero = Fuero, prioridad = Prioridad ,estado = Estado):
+        self.nroExp = nroExp
+        self.fuero = fuero
+        self.prioridad = prioridad
+        self.estado = estado
+
+    def __repr__(self):
+        printeo = "|| Nro Exp: " + str(self.nroExp) + "| tipo: " + str(self.fuero.name) + "| prioridad: " + str(self.prioridad.name) + "| estado: "+ str(self.estado.name) + " || "
+        return printeo
+    
+
+    def esNormal(self):
+        return self.prioridad == 0
+
+
+    def esUrgente(self):
+        return self.prioridad == 1
+
+
+
+class Juzgados:
+    
+    def __init__(self, nombreJuez = None):
+        self.colaUrgente = Colas()
+        self.colaNormal = Colas()
+        self.nombreJuez = nombreJuez
+        
+   
+    def __repr__(self):
+        printeo= "Juez: " + str(self.nombreJuez)+ "|" + " URGENTES: "+ str(self.colaUrgente) + " NORMALES: " + str(self.colaNormal)
+        return printeo
+    
+   
+    def recibirExpediente(self,exp):
+        if exp.prioridad == 0:
+            self.colaNormal.encolar(exp)
+        elif exp.prioridad ==1:
+            self.colaUrgente.encolar(exp)
+ 
+
+    def primerExpedienteATratar(self):
+        dato=None
+        if not self.colaUrgente.estaVacia():
+            dato=self.colaUrgente.ultimoElemento()
+        else:
+            dato=self.colaNormal.ultimoElemento()
+        return dato
+    
+    
+    
+    def tratarExpediente(self):
+        aux = None
+        if not self.colaUrgente.estaVacia():
+            aux = self.colaUrgente.desencolar()
+        else:
+            aux = self.colaNormal.desencolar()
+        return aux    
+   
+    
+    def cantidadTotalExpedientes(self):
+        return self.colaNormal.tamanioCola() + self.colaUrgente.tamanioCola()
+    
+       
+    def expedientesPorTipo(self):
+        return "cantidad exp cola normal: ", self.colaNormal.tamanioCola(), " cantidad exp cola urgentes: ", self.colaUrgente.tamanioCola()
+                
+   
+    
+    def esCritico(self):
+        return self.colaNormal.tamanioCola()>=50 or self.colaUrgente.tamanioCola() >= 50 
+    
+   
+    
+    def enJuicio(self):
+        sumaExpNormal=0
+        sumaExpUrg=0
+        for elementos in self.colaNormal:
+            if elementos.estado == 1:
+                sumaExpNormal+=1
+        
+        
+        for elementos in self.colaUrgente:
+            if elementos.estado == 1:
+                sumaExpUrg+=1
+        
+        return(sumaExpNormal+sumaExpUrg)
+                
+
+    def buscarExpediente(self,nroExp):
+        expendienteBuscado = None
+        for elementos in self.colaNormal:
+            if elementos.nroExp == nroExp:
+                expendienteBuscado = elementos
+        for elementos in self.colaUrgente :
+            if elementos.nroExp == nroExp:
+                expendienteBuscado = elementos
+
+        return expendienteBuscado
+    
+    
+    
+    
+    def eliminarExpediente(self,nroExp):
+
+        for elementos in self.colaNormal:
+            if elementos.nroExp == nroExp:
+                self.colaNormal.remove(elementos)
+
+        for elementos in self.colaUrgente:
+            if elementos.nroExp == nroExp:
+                self.colaUrgente.remove(elementos)
+    
+
+
+#    def cambiaDeEstado(self,nroExp):
+#            if self.buscarExpediente(nroExp) == nroExp :
+#                if self.estado=="normal":
+#                    self.estado = "urgente"
+#                    self.colaUrgente.recibirExpediente(nroExp)
+#                    self.colaNormal.eliminarExpediente(nroExp)
+#                elif self.estado == "urgente":
+#                    self.estado="normal"
+#                    self.colaUrgente.recibirExpediente(nroExp)
+#                    self.colaNormal.eliminarExpediente(nroExp)
+#                else:
+#                    Exception("El elemento no se encuentra en la cola")
+                
+                
+                
+#    def cambiaDeEstado(self,nroExp):
+#        
+#        aux = []
+#        
+#        for elementos in self.colaNormal:
+#            if elementos.nroExp == nroExp:
+#                elementos.prioridad = "urgente"
+#                aux.append(elementos)
+#                self.colaNormal.remove(elementos)
+#                self.colaUrgente.append(aux)
+#            
+#        for elementos in self.colaUrgente:
+#            if elementos.nroExp == nroExp:
+#                elementos.prioridad = "normal"
+#                aux.append(elementos)
+#                self.colaUrgente.remove(elementos)
+#                self.colaNormal.append(aux)
+                     
+            
+    def cambiaDeEstado(self,nroExp):
+         
+         aux = None
+         
+         if self.buscarExpediente(nroExp).nroExp == nroExp:
+             aux = self.buscarExpediente(nroExp)
+             if self.buscarExpediente(nroExp).prioridad == "urgente":
+                 self.eliminarExpediente(nroExp)
+                 aux.prioridad = "normal"
+                 self.recibirExpediente(aux)
+             else:
+                 self.eliminarExpediente(nroExp)
+                 aux.prioridad = "urgente"
+                 self.recibirExpediente(aux)
--- tp_juzgados.py
+++ tp_juzgados.py
@@ -1,228 +1,94 @@
-
-
 from Colas import *
 from Pila import *
 from Fuero import *
-
-class Expediente :
-    
-    def __init__(self, nroExp = 0 ,fuero= Fuero.Civil,prioridad = Prioridad.Normal ,estado = Estado.Juicio):
-        self.nroExp = nroExp
-        self.Fuero = fuero
-        self.Prioridad = prioridad
-        self.Estado = estado
-        
-   
-    
-    def __repr__(self):
-        printeo = "|| Nro Exp: " + str(self.nroExp) + "| tipo: " + str(self.Fuero) + "| prioridad: " + str(self.Prioridad) + "| estado: "+ str(self.Estado) + " || "
-        return printeo
-    
-
-    def esNormal(self):
-        return self.Prioridad == 0
-
-
-    def esUrgente(self):
-        return self.Prioridad == 1
-
-
-
-class Juzgados:
-    
-    
-    def __init__(self):
-        self.colaUrgente = []
-        self.colaNormal = []
-#        self.nombre=nombre
-        
-   
-    def __repr__(self):
-        printeo= "URGENTES: "+ str(self.colaUrgente) + " NORMALES: " + str(self.colaNormal)
-        return printeo
-    
-   
-    
-    def recibirExpediente(self,exp):
-        if exp.prioridad == "normal" :
-            self.colaNormal.append(exp)
-        elif exp.prioridad =="urgente" :
-            self.colaUrgente.append(exp)
-            
-    
-   
-    
-    def primerExpedienteATratar(self):
-        dato=None
-        if not len(self.colaUrgente)==0:
-            dato=self.colaUrgente[0]
-        else:
-            dato=self.colaNormal[0]
-        return dato
-    
-    
-    
-    def tratarExpediente(self):
-        if not len(self.colaUrgente) == 0:
-            self.colaUrgente.pop(0)
-        else:
-            self.colaNormal.pop(0)
-            
-   
-    
-    def cantidadTotalExpedientes(self):
-        return len(self.colaNormal) + len(self.colaUrgente)
-    
-    
-    
-    
-    def expedientesPorTipo(self):
-        return "cantidad exp cola normal: ", len(self.colaNormal), " cantidad exp cola urgentes: ", len(self.colaUrgente)
-                
-   
-    
-    def esCritico(self):
-        return len(self.colaNormal)>=50 or len(self.colaUrgente) >= 50 
-    
-   
-    
-    def enJuicio(self):
-        sumaExpNormal=0
-        sumaExpUrg=0
-        for elementos in self.colaNormal:
-            if elementos.estado == "juicio":
-                sumaExpNormal+=1
-        
-        
-        for elementos in self.colaUrgente :
-            if elementos.estado == "juicio":
-                sumaExpUrg+=1
-        
-        return(sumaExpNormal+sumaExpUrg)
-                
-
-    def buscarExpediente(self,nroExp):
-        expendienteBuscado = None
-        for elementos in self.colaNormal:
-            if elementos.nroExp == nroExp:
-                expendienteBuscado = elementos
-        for elementos in self.colaUrgente :
-            if elementos.nroExp == nroExp:
-                expendienteBuscado = elementos
-
-        return expendienteBuscado
-    
-    
-    
-    
-    def eliminarExpediente(self,nroExp):
-
-        for elementos in self.colaNormal:
-            if elementos.nroExp == nroExp:
-                self.colaNormal.remove(elementos)
-
-        for elementos in self.colaUrgente:
-            if elementos.nroExp == nroExp:
-                self.colaUrgente.remove(elementos)
-    
-
-
-#    def cambiaDeEstado(self,nroExp):
-#            if self.buscarExpediente(nroExp) == nroExp :
-#                if self.estado=="normal":
-#                    self.estado = "urgente"
-#                    self.colaUrgente.recibirExpediente(nroExp)
-#                    self.colaNormal.eliminarExpediente(nroExp)
-#                elif self.estado == "urgente":
-#                    self.estado="normal"
-#                    self.colaUrgente.recibirExpediente(nroExp)
-#                    self.colaNormal.eliminarExpediente(nroExp)
-#                else:
-#                    Exception("El elemento no se encuentra en la cola")
-                
-                
-                
-#    def cambiaDeEstado(self,nroExp):
-#        
-#        aux = []
-#        
-#        for elementos in self.colaNormal:
-#            if elementos.nroExp == nroExp:
-#                elementos.prioridad = "urgente"
-#                aux.append(elementos)
-#                self.colaNormal.remove(elementos)
-#                self.colaUrgente.append(aux)
-#            
-#        for elementos in self.colaUrgente:
-#            if elementos.nroExp == nroExp:
-#                elementos.prioridad = "normal"
-#                aux.append(elementos)
-#                self.colaUrgente.remove(elementos)
-#                self.colaNormal.append(aux)
-                     
-            
-    def cambiaDeEstado(self,nroExp):
-         
-         aux = None
-         
-         if self.buscarExpediente(nroExp).nroExp == nroExp:
-             aux = self.buscarExpediente(nroExp)
-             if self.buscarExpediente(nroExp).prioridad == "urgente":
-                 self.eliminarExpediente(nroExp)
-                 aux.prioridad = "normal"
-                 self.recibirExpediente(aux)
-             else:
-                 self.eliminarExpediente(nroExp)
-                 aux.prioridad = "urgente"
-                 self.recibirExpediente(aux)
+from Expedientes_Juzgados import *
                  
-             
-         
-         
-         
-    
+ #MAIN            
 
-expediente1 = Expediente(2,"civil", "normal", "investigacion")
-expediente2 = Expediente(4,"penal","urgente", "juicio")
-expediente3 = Expediente(23,"civil","urgente","investigacion")
-expediente4 = Expediente(40,"familia" ,"normal", "investigacion")
-expediente5 = Expediente(80,"penal", "normal", "investigacion")
-expediente6 = Expediente(83,"penal", "normal", "investigacion")
-expediente7 = Expediente(85,"penal", "normal", "juicio")
+expediente1 = Expediente(1, Fuero.Civil, Prioridad.Urgente, Estado.Juicio )
+expediente2 = Expediente(2,Fuero.Comercial, Prioridad.Urgente, Estado.Juicio)
+expediente3 = Expediente(3,Fuero.Familia, Prioridad.Normal, Estado.Investigacion)
+# expediente3 = Expediente(23,"civil","urgente","investigacion")
+# expediente4 = Expediente(40,"familia" ,"normal", "investigacion")
+# expediente5 = Expediente(80,"penal", "normal", "investigacion")
+# expediente6 = Expediente(83,"penal", "normal", "investigacion")
+# expediente7 = Expediente(85,"penal", "normal", "juicio")
 
+
+print(expediente1)
+print(expediente1.esNormal())
+print(expediente1.esUrgente())
+#pilas
 print()
-print("Contenido de los Juzgados: ")
-juzgado1 = Juzgados()
-
+# pilaUrgentes = Pila()
+# pilaNormal = Pila()
+juzgado1 = Juzgados("Juanjo")
+print() 
+print("Juzgado: ", juzgado1)
+print()
 juzgado1.recibirExpediente(expediente1)
 juzgado1.recibirExpediente(expediente2)
-#juzgado1.recibirExpediente(expediente3)
-#juzgado1.recibirExpediente(expediente4)
-#juzgado1.recibirExpediente(expediente5)
-#juzgado1.recibirExpediente(expediente6)
-#juzgado1.recibirExpediente(expediente7)
-
-
+juzgado1.recibirExpediente(expediente3)
+print()
 print(juzgado1)
 print()
-#print(juzgado1.primerExpedienteATratar())
-#juzgado1.tratarExpediente()
-
-print("La cantidad total de expedientes es : ",juzgado1.cantidadTotalExpedientes()) 
+print("El Primer Expediente A Tratar es: ", juzgado1.primerExpedienteATratar())
 print()
-print("la cantidad de expedientes por tipo es: ", juzgado1.expedientesPorTipo())
+#desencolo y saco de la fila
+#print("Tratar expediente: ", juzgado1.tratarExpediente())
 print()
-print("Es critico?: ", juzgado1.esCritico())
+print(juzgado1)
+print()
+print("La Cantidad Total De Exp. es:", juzgado1.cantidadTotalExpedientes())
+print()
+print(juzgado1.expedientesPorTipo())
+print() 
+print("Es Critico?: ", juzgado1.esCritico())  
 print()
 print("Cantidad de expedientes en Juicio: ", juzgado1.enJuicio())
-print()
 
-#buscar expediente numero:
-print(juzgado1.buscarExpediente(80))
 
-juzgado1.cambiaDeEstado(2)
+# expediente1 = Expediente(2,"civil", "normal", "investigacion")
+# expediente2 = Expediente(4,"penal","urgente", "juicio")
+# expediente3 = Expediente(23,"civil","urgente","investigacion")
+# expediente4 = Expediente(40,"familia" ,"normal", "investigacion")
+# expediente5 = Expediente(80,"penal", "normal", "investigacion")
+# expediente6 = Expediente(83,"penal", "normal", "investigacion")
+# expediente7 = Expediente(85,"penal", "normal", "juicio")
 
-print(juzgado1)
+# print()
+# print("Contenido de los Juzgados: ")
+# juzgado1 = Juzgados()
+
+# juzgado1.recibirExpediente(expediente1)
+# juzgado1.recibirExpediente(expediente2)
+# #juzgado1.recibirExpediente(expediente3)
+# #juzgado1.recibirExpediente(expediente4)
+# #juzgado1.recibirExpediente(expediente5)
+# #juzgado1.recibirExpediente(expediente6)
+# #juzgado1.recibirExpediente(expediente7)
+
+
+# print(juzgado1)
+# print()
+# #print(juzgado1.primerExpedienteATratar())
+# #juzgado1.tratarExpediente()
+
+# print("La cantidad total de expedientes es : ",juzgado1.cantidadTotalExpedientes()) 
+# print()
+# print("la cantidad de expedientes por tipo es: ", juzgado1.expedientesPorTipo())
+# print()
+# print("Es critico?: ", juzgado1.esCritico())
+# print()
+# print("Cantidad de expedientes en Juicio: ", juzgado1.enJuicio())
+# print()
+
+# #buscar expediente numero:
+# print(juzgado1.buscarExpediente(80))
+
+# juzgado1.cambiaDeEstado(2)
+
+# print(juzgado1)
 
 
 
