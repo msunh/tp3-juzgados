86c2fedbaa1c0d0120e20bd279dca5128cbe257b
msunh <msalto.unh@gmail.com>
2020-06-01 16:09:22 -0300

    inicio

--- UrgentesNormales.py
+++
@@ -1,2 +0,0 @@
-class normales:
-	__def__
---
+++ colas.py
@@ -0,0 +1,22 @@
+class Colas:
+    
+    def __init__(self):
+        self.cola=[] # creo la cola
+    
+    def __repr__(self): #imprime la cola con su contenido
+        return str(self.cola)    
+    
+    def encolar(self,elemento): #agrega un elemento a la cola
+        
+    
+    
+        
+    def vaciarCola(self):
+        self.cola.clear() #eliminar todo el contenido de la misma
+        
+            
+           
+     
+
+ 
+ 
